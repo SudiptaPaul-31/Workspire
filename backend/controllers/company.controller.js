e82e3331ff39801db61a7174b74d115a6afac75b
sudiptapaul <psudipta858@gmail.com>
2024-09-27 18:56:01 +0530

    Company admin page added

--- backend/controllers/company.controller.js
+++ backend/controllers/company.controller.js
@@ -1,4 +1,6 @@
 import {Company} from "../models/company.model.js"
+import getDataUri from './../utils/datauri.js';
+import cloudinary from './../utils/cloudinary.js';
 
 //Register company
 export const registerCompany = async(req, res) => {
@@ -79,8 +81,11 @@ export const updateCompany = async(req, res) => {
         const {name, description, website, location} = req.body;
         const file = req.file;
         //Cloudinary part
+        const fileUri = getDataUri(file);
+        const cloudResponse = await cloudinary.uploader.upload(fileUri.content);
+        const logo = cloudResponse.secure_url;
 
-        const updateData = {name, description, website, location};
+        const updateData = {name, description, website, location, logo};
         
         const company = await Company.findByIdAndUpdate(req.params.id, updateData, {new: true});
 
---
+++ frontend/src/components/admin/CompaniesTable.jsx
@@ -0,0 +1,66 @@
+import React from "react";
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "../ui/table";
+import { Avatar, AvatarImage } from "../ui/avatar";
+import { Popover, PopoverContent, PopoverTrigger } from "../ui/popover";
+import { Edit2, MoreHorizontal } from "lucide-react";
+import { useSelector } from "react-redux";
+
+const CompaniesTable = () => {
+  const { companies = [] } = useSelector((store) => store.company); // Default to empty array
+
+  return (
+    <div>
+      <Table>
+        <TableCaption>A list of your recent registered companies</TableCaption>
+        <TableHeader>
+          <TableRow>
+            <TableHead>Logo</TableHead>
+            <TableHead>Name</TableHead>
+            <TableHead>Date</TableHead>
+            <TableHead className="text-right">Action</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {companies.length <= 0 ? (
+            <span>You haven't registered any company yet.</span>
+          ) : (
+            companies.map((company) => (
+              <TableRow key={company._id}>
+                <TableCell>
+                  <Avatar>
+                    <AvatarImage src={company.logo} />
+                  </Avatar>
+                </TableCell>
+                <TableCell>{company.name}</TableCell>
+                <TableCell>{company.createdAt.split("T")[0]}</TableCell>
+                <TableCell className="text-right cursor-pointer">
+                  <Popover>
+                    <PopoverTrigger>
+                      <MoreHorizontal />
+                    </PopoverTrigger>
+                    <PopoverContent className="w-20">
+                      <div className="flex items-center gap-2 w-fit cursor-pointer">
+                        <Edit2 className="w-4" />
+                        <div>Edit</div>
+                      </div>
+                    </PopoverContent>
+                  </Popover>
+                </TableCell>
+              </TableRow>
+            ))
+          )}
+        </TableBody>
+      </Table>
+    </div>
+  );
+};
+
+export default CompaniesTable;
