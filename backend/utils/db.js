93e49188e6f6796163d3f46256030308c7bfac3b
sudiptapaul <psudipta858@gmail.com>
2025-04-11 23:28:24 +0530

    build changes

--- backend/utils/db.js
+++ backend/utils/db.js
@@ -1,11 +1,13 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
 
 const connectDB = async () => {
-  try {
-    await mongoose.connect(process.env.MONGO_URI);
-  } catch (error) {
-    console.log(error);
-  }
+    try {
+        const conn = await mongoose.connect(process.env.MONGO_URI);
+        console.log(`MongoDB connected: ${conn.connection.host}`);
+    } catch (error) {
+        console.error(`Error connecting to DB: ${error.message}`);
+        process.exit(1);
+    }
 };
 
 export default connectDB;
